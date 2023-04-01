cb0c45f59b2b1fee6fcdba58209699b73daf16fd
manexport <manexport@yeah.net>
2023-04-01 20:13:47 +0800

    module init.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/manexport/test_module
+
+go 1.20
---
+++ test_module.go
@@ -0,0 +1,7 @@
+package testmodule
+
+import "fmt"
+
+func Saygo(name string) string {
+	return fmt.Sprintln("Hi,%s let us go", name)
+}
